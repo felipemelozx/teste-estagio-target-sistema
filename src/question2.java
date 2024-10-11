e23b26dd57c381253137f3f047751356e5ab06f7
Felipe <felipemelozx@gmail.com>
2024-10-11 11:11:58 -0300

    feat: answer to question 2

--- src/question2.java
+++ src/question2.java
@@ -1,2 +1,31 @@
+
+/*
+Escreva um programa que verifique, em uma string, a existência da letra ‘a’,
+seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela ocorre.
+*/
+
+
+import java.util.Scanner;
+
 public class question2 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Informe sua string por favor.");
+
+    StringBuffer st = new StringBuffer(scan.nextLine());
+
+    int count = 0;
+
+    for(int i = 0; i < st.length(); i++){
+      if(st.charAt(i) == 'a' || st.charAt(i) == 'A' ){
+        count++;
+      }
+    }
+    if (count == 0){
+      System.out.println("Não contem a letra A ou a na sua string.");
+      return;
+    }
+    System.out.println("Na sua string contem " + count + " letras A ou a.");
+  }
 }
